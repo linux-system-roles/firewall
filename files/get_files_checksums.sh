9bfb00554ba3bcd55fcd315d9bd972968d317b6b
Rich Megginson <rmeggins@redhat.com>
2023-07-26 10:04:41 -0600

    fix: error when running with check mode and previous: replaced
    Cause: The role was not checking to see if any files would be
    changed when using `previous: replaced` in check mode.  The variables
    that should have been set were not set in check mode.
    
    Consequence: The role would give an error about undefined variables.
    
    Fix: A new check has been added for check mode, to see if any files
    would be changed by `previous: replaced`.  The check for
    `firewalld.conf` looks at the rpm database to determine if the file
    has been changed from the version shipped in the package.  If files
    would not be changed by using `previous: replaced`, then the role
    will also now correctly report if any settings would be changed.
    
    Result: The role works in check mode when using `previous: replaced`
    
    Signed-off-by: Rich Megginson <rmeggins@redhat.com>

--- files/get_files_checksums.sh
+++ files/get_files_checksums.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 python_cmd="$1"
 firewall_conf_root="${2:-/etc/firewalld}"
 remove="${3:-false}"
+package="${4:-}"
+firewall_usr_lib="${5:-}"
 
 listfile=$(mktemp)
 firewallconf=$(mktemp)
@@ -13,24 +15,44 @@ trap "rm -f $listfile $firewallconf" EXIT
 
 find "$firewall_conf_root" -name \*.xml | while read -r file; do
     cksum=$(xmllint --c14n "$file" | sha256sum | awk '{print $1}')
-    echo "$cksum" "$file"
+    if [ -n "$firewall_usr_lib" ]; then
+        usr_lib_file="${firewall_usr_lib}${file##"$firewall_conf_root"}"
+        if [ -f "$usr_lib_file" ]; then
+            cksum_usr_lib=$(xmllint --c14n "$usr_lib_file" | sha256sum | awk '{print $1}')
+            if [ "$cksum" != "$cksum_usr_lib" ]; then
+                echo "$cksum" "$file"
+            fi
+        else
+            echo "$cksum" "$file"
+        fi
+    else
+        echo "$cksum" "$file"
+    fi
 done > "$listfile"
 
-if [ -f "$firewall_conf_root/firewalld.conf" ]; then
-    cp "$firewall_conf_root/firewalld.conf" "$firewallconf"
-    "$python_cmd" -c 'import os, sys
+orig_conf="$firewall_conf_root/firewalld.conf"
+remove_firewall_conf=true
+if [ -f "$orig_conf" ]; then
+    if [ -z "$package" ] || rpm -V "$package" | grep -q "c ${orig_conf}$"; then
+        cp "$orig_conf" "$firewallconf"
+        "$python_cmd" -c 'import os, sys
 from firewall.core.io.firewalld_conf import firewalld_conf
 fc = firewalld_conf(sys.argv[1])
 fc.read(); os.unlink(sys.argv[1])
 fc.write()
 ' "$firewallconf"
-    cksum=$(sha256sum "$firewallconf" | awk '{print $1}')
-    echo "$cksum" "$firewall_conf_root/firewalld.conf" >> "$listfile"
+        cksum=$(sha256sum "$firewallconf" | awk '{print $1}')
+        echo "$cksum" "$orig_conf" >> "$listfile"
+    else
+        remove_firewall_conf=false
+    fi
 fi
 
 if [ "${remove:-false}" = true ]; then
     find "$firewall_conf_root" -name \*.xml -exec rm -f {} \;
-    rm -f "$firewall_conf_root/firewalld.conf"
+    if [ "$remove_firewall_conf" = true ]; then
+        rm -f "$orig_conf"
+    fi
     if [ -s "$listfile" ] ; then
         firewall-cmd --reload > /dev/null
     fi
